ee6d51c3147c6b4dbe8d6c7f3c3059a3605b8ccd
AntonMinaychev <mail.box42@yandex.ru>
2022-07-08 10:35:03 +0600

    Seminar 6

---
+++ Ex1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("How many numbers do you want to enter?"); 
+int a = int.Parse(Console.ReadLine());
+// Возможно, я неправильно понял условие задачи, и программа не должна спрашивать число М у пользователя.
+// Преобразовать введенную пользователем строку в числа (разделенные пробелами) можно, например, так:
+// int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int[] Array = new int[a];
+int counter = 0;
+int even = 0;
+
+while (counter < a)
+{
+ Console.WriteLine("Enter the number");
+ Array[counter] = int.Parse(Console.ReadLine());
+    if (Array[counter]%2 == 0)
+        {
+        even++;    
+        counter++;
+        }
+    else 
+        counter++;
+}
+
+Console.WriteLine($"You entered {even} even numbers");
---
+++ Ex2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Enter k1");
+double k1 = Double.Parse(Console.ReadLine());
+Console.WriteLine("Enter b1");
+double b1 = Double.Parse(Console.ReadLine());
+Console.WriteLine("Enter k2");
+double k2 = Double.Parse(Console.ReadLine());
+Console.WriteLine("Enter b2");
+double b2 = Double.Parse(Console.ReadLine());
+double x, y; 
+ 
+// Нахождение точки пересечения осуществляется с помощью системы уравнений, из нее в явном виде выразить нужно х и у
+
+x = (b2 - b1) / (k1  - k2);
+y = k1 * x + b1;
+ 
+Console.WriteLine($"Cross in: ({x};{y})");
